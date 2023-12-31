0a400bd5b797d643063285040d59597351256110
Ayoub Ritoub <66039193+Ayoub010001@users.noreply.github.com>
2023-12-31 22:42:05 +0100

    Welcome Visite & filter editing

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,10 @@ function Hero() {
     <section className='hero'>
         <div className='Hero-section'>
             <div className='Hero-main'>
-                <h1>Discover new places to visit</h1>
+              {/*
+                <h1>Discover new places to visit</h1>*/}
+                <h1 className="welcome"> WELCOME TO</h1>
+                <h1 className='visit2030'>Visit2030</h1>
                 <p>Four Years After USA-2026, FWC is Back. and we will assist you to discover new places near to stadium in Morocco, Spain and Portugal during the world cup 2030.</p>
             </div>
 
--- src/components/PlacesContainer.js
+++ src/components/PlacesContainer.js
@@ -5,9 +5,115 @@ import StadiumInfo from './StadiumInfo'
 
 
 function PlacesContainer({Stadium}) {
+    const originalPlaceVisit = Stadium.places;
+    const [placesVisit, setPlacesVisit] = useState(originalPlaceVisit);
+    const [selectedReligion, setSelectedReligion] = useState("Religion");
+    const [selectedCategory, setSelectedCategory] = useState("Categories");
+    const [selectedDistance, setSelectedDistance] = useState("Distance");
 
     const Categories = ["Religious Monument","Architectural Landmark","Garden","Public Square","Shopping","Cultural Attraction","Historical Landmark","Public Square","entertainment","Art","Art Museum","Natural view","Beach"];
-    const Religion = ["Islamic","Christian"];
+    const Religion = ["Islamic","Christian", "Catholic", "Eastern Orthodox Christianity"];
+    const Distance = ["From Far to Close","From Close to Far"];
+
+
+    
+    function applyFilters() {
+        let filteredPlaces = originalPlaceVisit;
+    
+        // Apply religion filter
+        if (selectedReligion !== "Religion") {
+          filteredPlaces = filteredPlaces.filter((place) => place["Religious Affiliation"] === selectedReligion);
+        }
+    
+        // Apply category filter
+        if (selectedCategory !== "Categories") {
+          filteredPlaces = filteredPlaces.filter((place) => place["category"].includes(selectedCategory));
+        }
+    
+        // Apply distance filter
+        if (selectedDistance !== "Distance") {
+          filteredPlaces = selectedDistance === "From Close to Far"
+            ? filteredPlaces.slice().sort((placeA, placeB) => placeA.distance - placeB.distance)
+            : filteredPlaces.slice().sort((placeA, placeB) => placeB.distance - placeA.distance);
+        }
+    
+        setPlacesVisit(filteredPlaces);
+      }
+    
+      function handleChangeReligion(e) {
+        setSelectedReligion(e.target.value);
+      }
+    
+      function handleChangeCategories(e) {
+        setSelectedCategory(e.target.value);
+      }
+    
+      function handleChangeDistance(e) {
+        setSelectedDistance(e.target.value);
+      }
+    
+      return (
+        <section className='places--container'>
+          <main className='places--content'>
+            <StadiumInfo selectedStadium={Stadium} />
+    
+            <h1>Places To Visit</h1>
+            <form>
+              <div className='filterContainer'>
+              <select className='badge input--Select' value={selectedReligion} onChange={handleChangeReligion}>
+    <option value={"Religion"}>Religion</option>
+    {Religion.map((religion) => (
+      <option key={religion} value={religion}>
+        {religion}
+      </option>
+    ))}
+  </select>
+
+  <select className='badge input--Select' value={selectedCategory} onChange={handleChangeCategories}>
+    <option value={"Categories"}>Categories</option>
+    {Categories.map((category) => (
+      <option key={category} value={category}>
+        {category}
+      </option>
+    ))}
+  </select>
+
+  <select className='badge input--Select' value={selectedDistance} onChange={handleChangeDistance}>
+    <option value={"Distance"}>Distance</option>
+    {Distance.map((distance) => (
+      <option key={distance} value={distance}>
+        {distance}
+      </option>
+    ))}
+  </select>
+                <button type="button" className="filterButton" onClick={applyFilters}>
+                  Apply Filters
+                </button>
+                <button type="button" className="resetButton" onClick={() => {
+                  setSelectedReligion("Religion");
+                  setSelectedCategory("Categories");
+                  setSelectedDistance("Distance");
+                  setPlacesVisit(originalPlaceVisit);
+                }}>
+                  Reset Filters
+                </button>
+              </div>
+            </form>
+            {placesVisit.map((place, index) => (
+              <PlaceSection place={place} key={index} />
+            ))}
+          </main>
+        </section>
+      );
+
+
+
+
+}
+    /*
+
+    const Categories = ["Religious Monument","Architectural Landmark","Garden","Public Square","Shopping","Cultural Attraction","Historical Landmark","Public Square","entertainment","Art","Art Museum","Natural view","Beach"];
+    const Religion = ["Islamic","Christian", "Catholic", "Eastern Orthodox Christianity"];
     const Distance = ["From Far to Close","From Close to Far"];
 
     // States
@@ -94,6 +200,7 @@ function PlacesContainer({Stadium}) {
                         }
                     </select>
                 </div>
+                
             </form>
             {
                 placesVisit.map((place, index)=>{
@@ -105,5 +212,5 @@ function PlacesContainer({Stadium}) {
     </section>
   )
 }
-
+*/
 export default PlacesContainer
